fe9f9ab7bb5387b98b24a6f7d0820e1053e1561e
algoprog <algoprog@users.noreply.github.com>
2018-05-11 04:54:07 +0300

    Model update

--- backend/model.py
+++ backend/model.py
@@ -1,7 +1,8 @@
 import numpy
 import pandas
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, BatchNormalization
+from keras.optimizers import Adam
 from keras.utils import np_utils
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
@@ -11,34 +12,30 @@ seed = 7
 numpy.random.seed(seed)
 
 # load dataset
-dataframe = pandas.read_csv("iris.csv", header=None)
+dataframe = pandas.read_csv("../data/data.txt", header=None)
 dataset = dataframe.values
-X = dataset[:, 0:4].astype(float)
-Y = dataset[:, 4]
-
-# encode class values as integers
-encoder = LabelEncoder()
-encoder.fit(Y)
-encoded_Y = encoder.transform(Y)
-# convert integers to dummy variables (i.e. one hot encoded)
-dummy_y = np_utils.to_categorical(encoded_Y)
+X = dataset[:, 0:466].astype(float)
+Y = dataset[:, 466:1190]
 
 
 # define baseline model
 def baseline_model():
     # create model
     model = Sequential()
-    model.add(Dense(8, input_dim=4, activation='relu'))
-    model.add(Dense(3, activation='softmax'))
+    model.add(Dense(200, input_dim=466, activation='relu'))
+    #model.add(BatchNormalization())
+    model.add(Dense(100, activation='relu'))
+    #model.add(BatchNormalization())
+    model.add(Dense(724, activation='softmax'))
     # Compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.01), metrics=['accuracy'])
     return model
 
 
 model = baseline_model();
 
 # Fit the model
-history = model.fit(X, dummy_y, epochs=100, batch_size=10, verbose=1)
+history = model.fit(X, Y, epochs=100, batch_size=10, verbose=1)
 
 # save model
 model.save('my_model.h5')
